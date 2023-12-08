7a41c76a8844dfd1f8828a743c87aa15c2850d24
Anhtuan2190 <tuanva2190@gmail.com>
2023-12-08 20:13:28 +0700

    Revert "element commit searchPage"
    This reverts commit ba41e1e16d5b8936868e02e1e8d42d86be43747a.

--- src/components/search-page/SearchPage.jsx
+++ src/components/search-page/SearchPage.jsx
@@ -1,37 +1,11 @@
 import React from "react";
 import "./SearchPage.scss";
-import { Button, Col, Container, Row } from "react-bootstrap";
-import SideBar from "../side-bar/SideBar";
-import InputGroupText from "react-bootstrap/esm/InputGroupText";
 
 const SearchPage = () => {
   return (
-    <Container fluid className="spotifyWepapp d-grid">
-      <Row className="topRow">
-        <Col md={2} lg={2} className="sideBarCol">
-          <SideBar />
-        </Col>
-        <Col md={10} lg={10}>
-          <div className="search">
-            <header className="searchHeader d-flex justify-content-between">
-              <div className="mainHeaderLeft">
-                <Button className="searchHeaderBtn">
-                  <i className="fa-solid fa-chevron-left"></i>
-                </Button>
-                <Button className="searchHeaderBtn">
-                  <i className="fa-solid fa-chevron-right"></i>
-                </Button>
-              </div>
-              <div className="searchBtn d-flex justifify-content-between">
-                <form >
-                
-                </form>
-              </div>
-            </header>
-          </div>
-        </Col>
-      </Row>
-    </Container>
+    <>
+      <div>SearchPage</div>
+    </>
   );
 };
 
