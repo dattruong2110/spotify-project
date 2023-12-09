bec3a856814e4ed7c0f7d5c39a14be3bbce99944
Trương Tấn Đạt <tandattruong2110@gmail.com>
2023-12-09 15:00:45 +0700

    Feature/SPOT-2: User can see sign up & sign in UI

--- src/router/AppRoutes.jsx
+++ src/router/AppRoutes.jsx
@@ -2,12 +2,18 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "../components/home-page/HomePage";
 import SearchPage from "../components/search-page/SearchPage";
+import SignUp from "../components/sign-up/SignUp";
+import Login from "../components/login/Login";
+import PasswordReset from "../components/password-reset/PasswordReset";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/search" element={<SearchPage />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/reset-password" element={<PasswordReset />} />
     </Routes>
   );
 };
