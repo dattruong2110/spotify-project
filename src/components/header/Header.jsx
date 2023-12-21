0c07005054857e34de16470e39d7667944cc99c5
Huỳnh Nguyễn <nguyenh165201@gmail.com>
2023-12-21 23:27:33 +0700

    FixBug: HEARDER & APPROUTER

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./header.scss";
+import "./Header.scss";
 import { NavLink, useNavigate } from "react-router-dom";
 import { Button } from "react-bootstrap";
 
@@ -15,6 +15,14 @@ const Header = ({ isPlaylistPage, showPlayButton }) => {
               <button type="button" class="fa fas fa-chevron-left"></button>
               <button type="button" class="fa fas fa-chevron-right"></button>
             </div>
+            {showPlayButton && (
+              <div className="d-flex">
+                <Button className="play-btn">
+                  <i className="fa fa-play"></i>
+                </Button>
+                <span className="header-playlist-title">lofi beats</span>
+              </div>
+            )}
           </div>
 
           <div class="navbar">
--- src/router/AppRoutes.jsx
+++ src/router/AppRoutes.jsx
@@ -10,9 +10,6 @@ import PlaylistArtist from "../components/playlist-artist/PlaylistArtist";
 import Playlist from "../components/playlist/Playlist";
 import Premium from "../components/premium-page/Premium";
 import Download from "../components/download-page/Download";
-import SideBarAfterLogin from "../components/side-bar/sidebar-afterlogin/SideBarAfterLogin";
-import FooterPlayMusic from "../components/footer/footer-playmusic/FooterPlayMusic";
-import HeaderAccount from "../components/header/hearder-account/HeaderAccount";
 import AccountManage from "../components/account-manage/AccountManage";
 import HomePageAfterLogin from "../components/home-page-after-login/HomePageAfterLogin";
 
@@ -26,9 +23,6 @@ const AppRoutes = () => {
       <Route path="/playlist" element={<Playlist />} />
       <Route path="/premium-page" element={<Premium />} />
       <Route path="/download-page" element={<Download />} />
-      <Route path="/sidebar-afterlogin" element={<SideBarAfterLogin />} />
-      <Route path="/header-account" element={<HeaderAccount />} />
-      <Route path="/footer-playmusic" element={<FooterPlayMusic />} />
       <Route path="/account-manage" element={<AccountManage />} />{" "}
       <Route path="/" element={<LandingPage />} />
       <Route path="/home-page" element={<HomePage />} />
