482a5969fc8d96eb5596b6cedf0f4ae6c7d3bd20
Trương Tấn Đạt <tandattruong2110@gmail.com>
2024-01-05 15:30:24 +0700

    Feature/SPOT-46: Implement Music Playback Feature

--- src/constants/Credentials.js
+++ src/constants/Credentials.js
@@ -1,10 +1,7 @@
 const Credentials = () => {
   return {
-    // ClientId: "4df3e809260b476ca74bec81c9873ddb",
-    // ClientSecret: "e949141673d54db9ad2f3e0b8c09af25",
-
-    ClientId: "c1eab79c58824381b38eadd1d3aff129",
-    ClientSecret: "0b4433ceb56246c1894f466829f4bfd1",
+    ClientId: "4df3e809260b476ca74bec81c9873ddb",
+    ClientSecret: "e949141673d54db9ad2f3e0b8c09af25",
   };
 };
 
